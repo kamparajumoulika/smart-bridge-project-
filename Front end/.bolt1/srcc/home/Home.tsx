933ada385b2a4d8509e7a9be7388b0a06cde6666
kamparajumoulika <22x51a0551@srecnandyal.edu.in>
2026-02-28 20:50:55 +0530

    Add Home component with Hero and Features

--- Front end/.bolt1/srcc/home/Home.tsx
+++ Front end/.bolt1/srcc/home/Home.tsx
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import { Hero } from './Hero';
+import { Features } from './Features';
+
+export function Home() {
+  return (
+    <div>
+      <Hero />
+      <Features />
+    </div>
+  );
+}
+
